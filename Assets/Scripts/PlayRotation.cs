094745080ee5930728ede7bedfe375687c7d4155
KIANALLEN-1 <kian.allen@student.uts.edu.au>
2024-09-18 13:05:38 +1000

    init

---
+++ Assets/Scripts/PlayRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRotation : MonoBehaviour
+{
+    public Animator animatorController;
+    public AudioClip buttonSound;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = buttonSound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (audioSource != null && buttonSound != null)
+            {
+                audioSource.Play();
+            }
+            if(animatorController != null)
+            {
+                animatorController.SetTrigger("RotateParam");
+
+            }
+
+        }
+    }
+}
